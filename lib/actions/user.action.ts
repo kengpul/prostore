88b542b5a71c0373aa0815ce17b185424dbc9149
kengpul <paulpulgares24@gmail.com>
2025-02-15 11:38:49 +0800

    authentication with next auth

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { signIn, signOut } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { hashSync } from "bcrypt-ts-edge";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { signInFormSchema, signUpFormSchema } from "../validators";
+import { formatError } from "../utils";
+
+export const signInWithCredentials = async (prevState: unknown, formData: FormData) => {
+    try {
+        const user = signInFormSchema.parse({
+            email: formData.get("email"),
+            password: formData.get("password")
+        })
+
+        await signIn("credentials", user)
+
+        return { success: true, message: "Signed in successfully" }
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        return { success: false, message: "Invalid email or password" }
+    }
+}
+
+export const signOutUserr = async () => {
+    await signOut();
+}
+
+export const signUp = async (prevstate: unknown, formData: FormData) => {
+    try {
+        const user = signUpFormSchema.parse({
+            name: formData.get("name"),
+            email: formData.get("email"),
+            password: formData.get("password"),
+            confirmPassword: formData.get("confirmPassword")
+        })
+
+        const plainPassword = user.password;
+        user.password = hashSync(user.password, 10)
+
+        await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password
+            }
+        })
+
+        await signIn("credentials", {
+            email: user.email,
+            password: plainPassword
+        })
+
+        return { success: true, message: "Signed up successfully" }
+
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+        return { success: false, message: formatError(error) }
+    }
+}
